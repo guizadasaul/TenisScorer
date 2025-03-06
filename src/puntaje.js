3a3f53aa1f8b6fb95cf3e5f4c262e6c05651964b
guizadasaul <saulguizada@Sauls-MacBook-Pro.local>
2025-03-06 09:40:08 -0400

    feat: 30 - 0

--- src/puntaje.js
+++ src/puntaje.js
@@ -1,7 +1,10 @@
-function puntuacion(a,b){
-    if(a === 1 && b === 0){
+function getScore(score1, score2){
+    if(score1 === 1 && score2 === 0){
     return "15 - love";
+    }
+    else if(score1 === 2 && score2 === 0){
+    return "30 - love";
     }
     return "love - love";
 }
-export default puntuacion;
+export default getScore;
--- src/puntaje.test.js
+++ src/puntaje.test.js
@@ -1,12 +1,16 @@
+import getScore from "./puntaje.js";
 import puntuacion from "./puntaje.js";
 import sumar from "./puntaje.js";
 
 describe("Marcador de puntaje de tenis", () => {
   it("En caso de que ambos jugadores tengan 0 el marcador es love, love", () => {
-    expect(puntuacion(0,0)).toEqual("love - love");
+    expect(getScore(0,0)).toEqual("love - love");
   });
-  it("En caso de que uno de los juagadores tenga 15-0", () => {
-    expect(puntuacion(1,0)).toEqual("15 - love");
+  it("En caso de que el primer jugador tenga 15-0", () => {
+    expect(getScore(1,0)).toEqual("15 - love");
+  });
+  it("En caso de que el primer jugador tenga 30-0", () => {
+    expect(getScore(2,0)).toEqual("30 - love");
   });
 });
 
