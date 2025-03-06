6436c788e94498603c5bb8986b4c54b493014cef
guizadasaul <saulguizada@Sauls-MacBook-Pro.local>
2025-03-06 09:32:35 -0400

    feat: 15 - 0

--- src/puntaje.js
+++ src/puntaje.js
@@ -1,4 +1,7 @@
-function puntuacion(){
-    return "love, love";
+function puntuacion(a,b){
+    if(a === 1 && b === 0){
+    return "15 - love";
+    }
+    return "love - love";
 }
 export default puntuacion;
--- src/puntaje.test.js
+++ src/puntaje.test.js
@@ -3,7 +3,10 @@ import sumar from "./puntaje.js";
 
 describe("Marcador de puntaje de tenis", () => {
   it("En caso de que ambos jugadores tengan 0 el marcador es love, love", () => {
-    expect(puntuacion(0,0)).toEqual("love, love");
+    expect(puntuacion(0,0)).toEqual("love - love");
+  });
+  it("En caso de que uno de los juagadores tenga 15-0", () => {
+    expect(puntuacion(1,0)).toEqual("15 - love");
   });
 });
 
